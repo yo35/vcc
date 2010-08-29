9fdf17e8f7c4764966761490e3d904d00455c2b6
Yoann Le Montagner <yo35@melix.net>
2010-08-29 14:06:45 +0000

    Renommage Timer

--- timer.cpp
+++ timer.cpp
@@ -1,41 +1,41 @@
 
-#include "abstracttimer.h"
+#include "timer.h"
 #include <glibmm/main.h>
 
-AbstractTimer::AbstractTimer() {
+Timer::Timer() {
 	mode = PAUSED;
 	curr_time = 0;
 	reset_counter();
-	Glib::signal_timeout().connect(sigc::mem_fun(*this, &AbstractTimer::on_timeout_elapses), 150);
+	Glib::signal_timeout().connect(sigc::mem_fun(*this, &Timer::on_timeout_elapses), 150);
 }
 
-void AbstractTimer::set_mode(Mode new_mode) {
+void Timer::set_mode(Mode new_mode) {
 	refresh_time();
 	mode = new_mode;
 	reset_counter();
 	refresh_widget();
 }
 
-AbstractTimer::Mode AbstractTimer::get_mode() const {
+Timer::Mode Timer::get_mode() const {
 	return mode;
 }
 
-void AbstractTimer::set_time(int new_time) {
+void Timer::set_time(int new_time) {
 	curr_time = new_time;
 	reset_counter();
 	refresh_widget();
 }
 
-int AbstractTimer::get_time() const {
+int Timer::get_time() const {
 	refresh_time();
 	return curr_time;
 }
 
-int AbstractTimer::get_raw_time() const {
+int Timer::get_raw_time() const {
 	return curr_time;
 }
 
-void AbstractTimer::refresh_time() const {
+void Timer::refresh_time() const {
 
 	// Si on est en pause, curr_time est supposé être à jour
 	if(mode==PAUSED)
@@ -50,13 +50,13 @@ void AbstractTimer::refresh_time() const {
 		curr_time = time_at_begin - diff;
 }
 
-bool AbstractTimer::on_timeout_elapses() {
+bool Timer::on_timeout_elapses() {
 	refresh_time();
 	refresh_widget();
 	return true;
 }
 
-void AbstractTimer::reset_counter() {
+void Timer::reset_counter() {
 	time_at_begin = curr_time;
 	time(&start_at);
 }
--- timer.h
+++ timer.h
@@ -1,42 +1,43 @@
 
-#ifndef ABSTRACTTIMER_H
-#define ABSTRACTTIMER_H
+#ifndef TIMER_H
+#define TIMER_H
 
 #include <ctime>
+#include <glibmm/object.h>
 
-class AbstractTimer {
+class Timer : public Glib::Object {
 
 public:
-	
+
 	// Le mode du timer
 	typedef enum {
 		INCREMENT,
 		DECREMENT,
 		PAUSED
 	} Mode;
-	
+
 	// Constructeur
-	AbstractTimer();
-	
+	Timer();
+
 	void set_mode(Mode new_mode);
 	Mode get_mode() const;
-	
+
 	void set_time(int new_time);
 	int  get_time() const;
-	
+
 protected:
-	
+
 	// Routine a surcharger pour la gestion de l'affichage
 	virtual void refresh_widget() = 0;
 	int get_raw_time() const;
-	
+
 private:
 
 	// Routines internes
 	void refresh_time() const;
 	bool on_timeout_elapses();
 	void reset_counter();
-	
+
 	// Données membres
 	Mode   mode;
 	mutable int curr_time;
