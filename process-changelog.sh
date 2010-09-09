0cbe37aa9b77f2afd626646b91216927cbad7bc2
Yoann Le Montagner <yo35@melix.net>
2010-09-09 18:29:30 +0000

    Script de correction du ChangeLog généré par svn2cl

---
+++ process-changelog.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+TMP_OUTPUT_FILE=tmp_output_file
+
+# Supprime les lignes concernant ChangeLog dans ChangLog
+sed -e '/* ChangeLog:/d' ChangeLog > $TMP_OUTPUT_FILE
+mv $TMP_OUTPUT_FILE ChangeLog
+
+# Modifie les noms des développeurs
+sed \
+	-e 's/\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)  yoann/\1  Yoann Le Montagner <yo35(at)melix(dot)net>/g' \
+	-e 's/\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)  Administrateur/\1  Yoann Le Montagner <yo35(at)melix(dot)net>/g' \
+	ChangeLog > $TMP_OUTPUT_FILE
+mv $TMP_OUTPUT_FILE ChangeLog
