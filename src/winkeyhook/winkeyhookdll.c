f5a954e55549b680ecbb9eed2ffdeb5b075c9952
Yoann Le Montagner <yo35@melix.net>
2010-09-14 18:10:48 +0000

    Ajout des copyrights en tête des fichiers source

--- src/winkeyhook/winkeyhookdll.c
+++ src/winkeyhook/winkeyhookdll.c
@@ -1,3 +1,21 @@
+/*
+ *    This file is part of Virtual Chess Clock, a chess clock software
+ *
+ *    Copyright (C) 2010  Yoann Le Montagner <yo35(at)melix(dot)net>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "winkeyhookdll.h"
 #include <windows.h>
@@ -8,16 +26,16 @@ HHOOK hHook = 0;
 LRESULT CALLBACK low_level_keyboard_proc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	PKBDLLHOOKSTRUCT kb_input_event;
-	
+
 	/* First check */
 	if(nCode<0)
 		return CallNextHookEx(0, nCode, wParam, lParam);
 	kb_input_event = (PKBDLLHOOKSTRUCT)lParam;
-	
+
 	/* If the event comes from a window key, don't do anything */
 	if(kb_input_event->vkCode==VK_LWIN || kb_input_event->vkCode==VK_RWIN)
 		return 1;
-	
+
 	/* Otherwise, forward the event */
 	else
 		return CallNextHookEx(0, nCode, wParam, lParam);
@@ -26,7 +44,7 @@ LRESULT CALLBACK low_level_keyboard_proc(int nCode, WPARAM wParam, LPARAM lParam
 int __declspec(dllexport) set_kbd_hook()
 {
 	HMODULE hCurrModule;
-	
+
 	/* Retrieve current module's handle */
 	hCurrModule = GetModuleHandle(HOOKDLL_FULL_NAME);
 	if(hCurrModule==NULL)
