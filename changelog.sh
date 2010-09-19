d72d1163241281486460ba57615a242b07597e36
Yoann Le Montagner <yo35@melix.net>
2010-09-19 12:42:16 +0000

    Debug

--- changelog.sh
+++ changelog.sh
@@ -11,7 +11,8 @@ mv $TMP_OUTPUT_FILE ChangeLog
 
 # Modifie les noms des développeurs
 sed \
-	-e 's/\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)  yoann/\1  Yoann Le Montagner <yo35(at)melix(dot)net>/g' \
-	-e 's/\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)  Administrateur/\1  Yoann Le Montagner <yo35(at)melix(dot)net>/g' \
+	-e 's/\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)  yoann$/\1  Yoann Le Montagner <yo35(at)melix(dot)net>/g' \
+	-e 's/\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)  Administrateur$/\1  Yoann Le Montagner <yo35(at)melix(dot)net>/g' \
+	-e 's/\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)  Yoann$/\1  Yoann Le Montagner <yo35(at)melix(dot)net>/g' \
 	ChangeLog > $TMP_OUTPUT_FILE
 mv $TMP_OUTPUT_FILE ChangeLog
