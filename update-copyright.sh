094759b7d2e81c5880594b90b57209f58d50cfa7
Yoann Le Montagner <yo35@melix.net>
2012-02-26 09:40:09 +0000

    MAJ copyright

--- update-copyright.sh
+++ update-copyright.sh
@@ -21,41 +21,53 @@
 ################################################################################
 
 
-# Fichiers sources
-SRC_FILES=`ls src/*.{h,cpp,inl} src/winkeyhook/*.c include/*.h`
+# Message de copyright
+COPYRIGHT_MESSAGE="Copyright (C) 2010-2012 Yoann Le Montagner <yo35(at)melix(dot)net>"
 
-# Nombre total de lignes
-STOC_TOTAL=`cat $SRC_FILES | wc -l`
+# Vérification copyright
+COPYRIGHT_MESSAGE=`echo $COPYRIGHT_MESSAGE | sed -n -e '/^Copyright/p'`
+LENGTH_MESSAGE=$[`echo $COPYRIGHT_MESSAGE | wc -c` - 1]
+if [ "$LENGTH_MESSAGE" -eq 0 ]; then
+	echo "Invalid copyright message"
+	exit
+elif [ "$LENGTH_MESSAGE" -gt 70 ]; then
+	echo "The message is too long"
+	exit
+fi
+COPYRIGHT_MESSAGE=`echo $COPYRIGHT_MESSAGE | sed -e 's/ /<SPACE>/g'`
 
-# Nombre de lignes non-vides
-STOC_NON_EMPTY=`cat $SRC_FILES | sed -e '
-	/^\s*$/d
-' | wc -l`
+# Fichier temporaire
+TMP_FILE="tmp_file"
 
-# Nombre de lignes avec commentaire seul
-STOC_COMM_CPP_ALONE=`cat $SRC_FILES | sed -n -e '
-	/^\s*\/\/.*$/p
-' | wc -l`
-STOC_COMM_C_ALONE=`cat $SRC_FILES | sed -n -e '
-	/^\s*\/\*.*\*\/\s*$/p
-' | wc -l`
+# Espaces de fin de ligne
+LENGTH_PADDING_C_FILE=$[80 - 8 - $LENGTH_MESSAGE]
+LENGTH_PADDING_SH_FILE=$[80 - 6 - $LENGTH_MESSAGE]
+LENGTH_PADDING_BAT_FILE=$[80 - 10 - $LENGTH_MESSAGE]
 
-# Nombre de lignes dans un bloc de commentaire (/* qqch */ sur plusieurs lignes)
-STOC_COMM_BLOC=`cat $SRC_FILES | sed -n -e '
-	/\/\*.*\*\//d
-	/\/\*/,/\*\// {
-		p
-	}
-' | wc -l`
+# Fonction générant les chaînes de padding
+function make_padder {
+	RETVAL=""
+	COUNTER=$1
+	while [ "$COUNTER" -gt 0 ]
+	do
+		RETVAL=$RETVAL"<SPACE>"
+		COUNTER=$[$COUNTER - 1]
+	done
+	echo $RETVAL
+}
+PADDER_PREFIX="<SPACE><SPACE><SPACE><SPACE>"
+PADDER_C_FILE=$(make_padder $LENGTH_PADDING_C_FILE)
+PADDER_SH_FILE=$(make_padder $LENGTH_PADDING_SH_FILE)
+PADDER_BAT_FILE=$(make_padder $LENGTH_PADDING_BAT_FILE)
 
-# Nombre total de lignes de commentaire
-STOC_COMM_ALL=$[$STOC_COMM_CPP_ALONE + $STOC_COMM_C_ALONE + $STOC_COMM_BLOC]
-
-# Code pure
-STOC_CODE_ONLY=$[$STOC_NON_EMPTY - $STOC_COMM_ALL]
-
-# Affichage
-echo -e "Total code lines:\t $STOC_TOTAL"
-echo -e "Non-empty lines: \t $STOC_NON_EMPTY\t ($[$STOC_NON_EMPTY*100/$STOC_TOTAL] %)"
-echo -e "Comment lines:   \t $STOC_COMM_ALL\t ($[$STOC_COMM_ALL*100/$STOC_TOTAL] %)"
-echo -e "Pure code lines: \t $STOC_CODE_ONLY\t ($[$STOC_CODE_ONLY*100/$STOC_TOTAL] %)"
+# MAJ des fichiers
+for FILE in $*
+do
+	echo "Updating "$FILE"..."
+	sed -i -e 's/^ \*.*Copyright.*\*/ *'$PADDER_PREFIX$COPYRIGHT_MESSAGE$PADDER_C_FILE'*/g' $FILE
+	sed -i -e 's/^#.*Copyright.*#/#'$PADDER_PREFIX$COPYRIGHT_MESSAGE$PADDER_SH_FILE'#/g' $FILE
+	sed -i -e 's/^rem #.*Copyright.*#/rem #'$PADDER_PREFIX$COPYRIGHT_MESSAGE$PADDER_BAT_FILE'#/g' $FILE
+	sed -i -e '/Copyright/ {
+		s/<SPACE>/ /g
+	}' $FILE
+done
