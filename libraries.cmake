050bda215786c853a64af8bf934eaaeee9cb090e
Yoann Le Montagner <yo35@melix.net>
2013-11-30 23:35:09 +0100

    Set up CMakeLists.txt for Qt

---
+++ libraries.cmake
@@ -0,0 +1,42 @@
+################################################################################
+#                                                                              #
+#    This file is part of Virtual Chess Clock, a chess clock software          #
+#                                                                              #
+#    Copyright (C) 2010-2012 Yoann Le Montagner <yo35(at)melix(dot)net>        #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version.                                       #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                              #
+################################################################################
+
+
+# Boost
+find_package(Boost COMPONENTS system filesystem)
+
+
+# Qt
+set(CMAKE_AUTOMOC ON)
+set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "/opt/yoann/Qt5.1.1/5.1.1/gcc_64/lib/cmake/Qt5Widgets") #TODO
+find_package(Qt5Widgets)
+
+
+# All librairies together
+set(all_INCLUDE_DIRS
+	${Boost_INCLUDE_DIRS}
+)
+set(all_LIBRARY_DIRS
+	${Boost_LIBRARY_DIRS}
+)
+set(all_LIBRARIES
+	${Boost_LIBRARIES}
+)
